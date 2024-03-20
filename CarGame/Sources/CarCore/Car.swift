0b2ff277f736de064aae0f25b4650f0bd7e77c6c
Davide De Rosa <keeshux@gmail.com>
2024-03-20 09:43:54 +0100

    Modularize 1: Components

--- CarGame/Sources/CarCore/Car.swift
+++ CarGame/Sources/CarCore/Car.swift
@@ -14,69 +14,92 @@ enum CarError: Error {
 }
 
 public struct Car {
-    public enum Direction {
-        case left
+    private static let numberOfWheels = 4
 
-        case right
-    }
+    private static let numberOfFrontWheels = 2
 
-    public private(set) var isStarted = false
+    private let brakeComponent: Brake
 
-    public private(set) var speed = 0
+    private var engine: Engine
 
-    public private(set) var angle = 0
+    public private(set) var wheels: [Wheel]
 
     public init() {
+        brakeComponent = Brake()
+        engine = Engine()
+        wheels = (0..<Self.numberOfWheels)
+            .map {
+                // only front wheels can steer
+                Wheel(canSteer: $0 < Self.numberOfFrontWheels)
+            }
     }
 
     public mutating func start() {
-        isStarted = true
+        engine.start()
     }
 
     public mutating func stop() throws {
         guard !isMoving else {
             throw CarError.carIsMoving
         }
-        isStarted = false
+        engine.stop()
+    }
+
+    public var isStarted: Bool {
+        engine.isOn
     }
 
     public var isMoving: Bool {
-        speed != 0
+        currentSpeed() != 0
     }
 
     public var isSteering: Bool {
-        angle != 0
+        currentAngle() != 0
     }
 
     public func currentSpeed() -> Int {
-        speed
+        let differentSpeeds = Set(wheels
+            .map(\.speed)
+        )
+        guard differentSpeeds.count == 1 else {
+            assertionFailure("Wheels have different speeds")
+            return 0
+        }
+        return differentSpeeds.first ?? 0
     }
 
     public func currentAngle() -> Int {
-        angle
+        let differentAngles = Set(wheels
+            .enumerated()
+            .filter { $0.offset < Self.numberOfFrontWheels }
+            .map(\.element.angle)
+        )
+        guard differentAngles.count == 1 else {
+            assertionFailure("Front wheels have different angles")
+            return 0
+        }
+        return differentAngles.first ?? 0
     }
 
     public mutating func accelerate() throws {
-        guard isStarted else {
+        guard engine.isOn else {
             throw CarError.carIsStopped
         }
-        speed += 10
+        wheels = wheels.copy {
+            $0.doAccelerate()
+        }
     }
 
     public mutating func brake() {
-        speed = max(0, speed - 10)
+        wheels = wheels.copy {
+            $0.doBrake(with: brakeComponent)
+        }
     }
 
-    public mutating func steer(direction: Direction) {
-        let delta: Int
-        switch direction {
-        case .left:
-            delta = -10
-
-        case .right:
-            delta = 10
+    public mutating func steer(direction: Wheel.Direction) {
+        wheels = wheels.copy {
+            $0.doSteer(direction: direction)
         }
-        angle += delta
     }
 }
 
@@ -88,6 +111,9 @@ extension Car: CustomStringConvertible {
         desc.append("\tisMoving = \(isMoving)")
         desc.append("\tcurrentSpeed = \(currentSpeed())")
         desc.append("\tcurrentAngle = \(currentAngle())")
+        wheels.enumerated().forEach {
+            desc.append("\twheel[\($0.offset)] = (speed: \($0.element.speed), angle: \($0.element.angle))")
+        }
         return desc.joined(separator: "\n")
     }
 }
---
+++ CarGame/Sources/CarCore/Components/Brake.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Davide De Rosa on 3/17/24.
+//
+
+import Foundation
+
+public struct Brake {
+    public let speedVariation: Int
+
+    public init() {
+        speedVariation = 10
+    }
+}
