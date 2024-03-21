7aa334373f4456bce32dbc9d64e61e3fda2e2d97
Davide De Rosa <keeshux@gmail.com>
2024-03-21 07:12:16 +0100

    Write proper unit tests

--- CarGame/Tests/CarCoreTests/CarTests.swift
+++ CarGame/Tests/CarCoreTests/CarTests.swift
@@ -10,33 +10,61 @@ import Foundation
 import XCTest
 
 final class CarTests: XCTestCase {
-    func testCar() throws {
+    func test_givenStoppedCar_whenStart_thenIsStarted() {
         var sut = Car()
+        sut.start()
+        XCTAssertTrue(sut.isStarted)
+    }
 
+    func test_givenStoppedCar_whenStop_thenIsStopped() throws {
+        var sut = Car()
         sut.start()
         XCTAssertTrue(sut.isStarted)
         try sut.stop()
         XCTAssertFalse(sut.isStarted)
+    }
 
+    func test_givenMovingCar_whenStop_thenFails() throws {
+        var sut = Car()
         sut.start()
         try sut.accelerate()
         XCTAssertThrowsError(try sut.stop())
-        sut.brake()
-        try sut.stop()
+    }
 
+    func test_givenStartedCar_whenAccelerate_thenSpeedIncreases() throws {
+        var sut = Car()
         sut.start()
-        XCTAssertTrue(sut.isStarted)
+        XCTAssertTrue(sut.isStarted) // out of scope
+
         try sut.accelerate()
         XCTAssertEqual(sut.currentSpeed(), 10)
         try sut.accelerate()
         XCTAssertEqual(sut.currentSpeed(), 20)
+    }
+
+    func test_givenStartedCar_whenBrake_thenSpeedDecreases() throws {
+        var sut = Car()
+        sut.start()
+        XCTAssertTrue(sut.isStarted) // out of scope
+
         try sut.accelerate()
-        XCTAssertEqual(sut.currentSpeed(), 30)
+        try sut.accelerate()
+        try sut.accelerate()
+        XCTAssertEqual(sut.currentSpeed(), 30) // out of scope
 
         sut.brake()
         XCTAssertEqual(sut.currentSpeed(), 20)
         sut.brake()
         XCTAssertEqual(sut.currentSpeed(), 10)
+    }
+
+    func test_givenStoppedCar_whenAccelerate_thenFails() throws {
+        var sut = Car()
+        XCTAssertThrowsError(try sut.accelerate())
+    }
+
+    func test_givenCar_whenSteer_thenAngleIsAltered() throws {
+        var sut = Car()
 
         sut.steer(direction: .left)
         XCTAssertTrue(sut.isSteering)
